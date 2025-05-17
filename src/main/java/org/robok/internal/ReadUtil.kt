217a846ca4d158158bcf114c34e8592965f34aae
Aquiles Trindade <devsuay@gmail.com>
2025-05-17 16:58:08 -0300

    chore: add jvmname

--- src/main/java/org/robok/internal/ReadUtil.kt
+++ src/main/java/org/robok/internal/ReadUtil.kt
@@ -1,3 +1,5 @@
+@file:JvmName("__INTERNAL_READ_UTIL__")
+
 package org.robok.internal
 
 import android.content.Context
@@ -5,12 +7,12 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun Context.readTextFromAndroidAssets(dir: String): String {
-  val builder = StringBuilder()
-  val reader = BufferedReader(InputStreamReader(assets.open(dir)))
-  var line: String? = null
-  while (reader.readLine().also { line = it } != null) {
-    builder.append(line).append("\n")
-  }
-  reader.close()
-  return builder.toString()
+    val builder = StringBuilder()
+    val reader = BufferedReader(InputStreamReader(assets.open(dir)))
+    var line: String?
+    while (reader.readLine().also { line = it } != null) {
+        builder.append(line).append("\n")
+    }
+    reader.close()
+    return builder.toString()
 }
